eeab600aa376122e9e29efc3429dbb6e04d1f9d6
Chris Lowe <clowe@luc.edu>
2020-08-08 21:00:53 -0600

    Updates

---
+++ precache-manifest.19249ad0cfa929b9ad187e7220cb4515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26770ed75419f29e62cb5b21a89e3b5d",
+    "url": "/ror2-max-stacks/index.html"
+  },
+  {
+    "revision": "665370006190dc3a2e24",
+    "url": "/ror2-max-stacks/static/css/main.c2cd61ec.chunk.css"
+  },
+  {
+    "revision": "c1234220c01999b3801c",
+    "url": "/ror2-max-stacks/static/js/2.cc43b96d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ror2-max-stacks/static/js/2.cc43b96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665370006190dc3a2e24",
+    "url": "/ror2-max-stacks/static/js/main.13a9fa20.chunk.js"
+  },
+  {
+    "revision": "ffb36ba4ff1caea11bd9",
+    "url": "/ror2-max-stacks/static/js/runtime-main.3a5169f0.js"
+  }
+]);
