974f9433f58cf83186003d5cb548ae248be4dc16
Chris Lowe <clowe@luc.edu>
2025-12-02 21:13:53 -0700

    random clean ups

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import ror1BossData from "../../data/ror1/boss.json";
-import ror1CommonData from "../../data/ror1/common.json";
-import ror1RareData from "../../data/ror1/rare.json";
-import ror1SpecialData from "../../data/ror1/special.json";
-import ror1UncommonData from "../../data/ror1/uncommon.json";
-import ror2BossData from "../../data/ror2/boss.json";
-import ror2CommonData from "../../data/ror2/common.json";
-import ror2LegendaryData from "../../data/ror2/legendary.json";
-import ror2LunarData from "../../data/ror2/lunar.json";
-import ror2UncommonData from "../../data/ror2/uncommon.json";
-import ror2VoidData from "../../data/ror2/void.json";
-import LastUpdated from "../lastUpdated/lastUpdated.js";
-import FilterableDropdown from "../itemFilterDropdown/itemFilterDropdown";
-import ItemList from "../itemList.js";
-import "../../fonts/BOMBARD_.ttf";
-import "semantic-ui-css/semantic.min.css";
-import { Grid, Button, Dropdown } from "semantic-ui-react"; // eslint-disable-line no-unused-vars
+import React, { useState } from 'react'
+import ror1BossData from '../../data/ror1/boss.json'
+import ror1CommonData from '../../data/ror1/common.json'
+import ror1RareData from '../../data/ror1/rare.json'
+import ror1SpecialData from '../../data/ror1/special.json'
+import ror1UncommonData from '../../data/ror1/uncommon.json'
+import ror2BossData from '../../data/ror2/boss.json'
+import ror2CommonData from '../../data/ror2/common.json'
+import ror2LegendaryData from '../../data/ror2/legendary.json'
+import ror2LunarData from '../../data/ror2/lunar.json'
+import ror2UncommonData from '../../data/ror2/uncommon.json'
+import ror2VoidData from '../../data/ror2/void.json'
+import LastUpdated from '../lastUpdated/lastUpdated.js'
+import FilterableDropdown from '../itemFilterDropdown/itemFilterDropdown'
+import ItemList from '../itemList.js'
+import '../../fonts/BOMBARD_.ttf'
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Button, Dropdown } from 'semantic-ui-react' // eslint-disable-line no-unused-vars
 
 const App = () => {
   const games = [
     {
-      key: "ror",
-      text: "Risk of Rain",
-      value: "Jenny Hess",
+      key: 'ror',
+      text: 'Risk of Rain',
+      value: 'Jenny Hess',
     },
     {
-      key: "ror2",
-      text: "Risk of Rain 2",
-      value: "Elliot Fu",
+      key: 'ror2',
+      text: 'Risk of Rain 2',
+      value: 'Elliot Fu',
     },
     {
-      key: "rorr",
-      text: "Risk of Rain Returns",
-      value: "Stevie Feliciano",
+      key: 'rorr',
+      text: 'Risk of Rain Returns',
+      value: 'Stevie Feliciano',
     },
-  ];
+  ]
 
   const ror1Data = [].concat(
     ror1CommonData,
@@ -42,7 +42,7 @@ const App = () => {
     ror1RareData,
     ror1BossData,
     ror1SpecialData
-  );
+  )
 
   const ror2Data = [].concat(
     ror2CommonData,
@@ -51,33 +51,33 @@ const App = () => {
     ror2BossData,
     ror2LunarData,
     ror2VoidData
-  );
+  )
 
-  const rorrData = [].concat();
+  const rorrData = [].concat()
 
-  const [rarity, setRarity] = useState("All");
-  const [stackType, setStackType] = useState("All");
-  const [gameData, setGameData] = useState(ror2Data);
-  const [game, setGame] = useState(2);
+  const [rarity, setRarity] = useState('All')
+  const [stackType, setStackType] = useState('All')
+  const [gameData, setGameData] = useState(ror2Data)
+  const [game, setGame] = useState(2)
 
   const handleRarityChange = (newRarityValue) => {
-    setRarity(newRarityValue);
-  };
+    setRarity(newRarityValue)
+  }
 
   const handleStackTypeChange = (newStackTypeValue) => {
-    setStackType(newStackTypeValue);
-  };
+    setStackType(newStackTypeValue)
+  }
 
   // handleChange = (e, { value }) => this.setState({ value })
   const handleGameChange = (e, { value }) => {
     // eslint-disable-line no-unused-vars
-    setGame(value);
+    setGame(value)
     if (game === 1) {
-      setGameData(ror1Data);
+      setGameData(ror1Data)
     } else if (game === 2) {
-      setGameData(ror2Data);
+      setGameData(ror2Data)
     } else {
-      setGameData(rorrData);
+      setGameData(rorrData)
     }
     // if (game === 1) {
     //   setGame(2)
@@ -88,7 +88,7 @@ const App = () => {
     // } else {
     //   setGameData(rorrData) // TODO: this doesn't work
     // }
-  };
+  }
 
   return (
     <>
@@ -129,7 +129,7 @@ const App = () => {
         game={game}
       />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,33 +1,33 @@
-import React from "react";
-import Item from "./item/item.js";
-import { Grid } from "semantic-ui-react";
+import React from 'react'
+import Item from './item/item.js'
+import { Grid } from 'semantic-ui-react'
 
 const ItemList = ({ itemData, rarityFilter, stackTypeFilter, game }) => (
   <Grid padded>
     {itemData
       .filter((item) => {
-        if (rarityFilter !== "All" && stackTypeFilter !== "All") {
+        if (rarityFilter !== 'All' && stackTypeFilter !== 'All') {
           return (
             item.Rarity === rarityFilter &&
             item.StackDetails.find(
               (detail) => detail.StackType === stackTypeFilter
             )
-          );
-        } else if (rarityFilter === "All" && stackTypeFilter !== "All") {
+          )
+        } else if (rarityFilter === 'All' && stackTypeFilter !== 'All') {
           return item.StackDetails.find(
             (detail) => detail.StackType === stackTypeFilter
-          );
-        } else if (rarityFilter !== "All" && stackTypeFilter === "All") {
-          return item.Rarity === rarityFilter;
+          )
+        } else if (rarityFilter !== 'All' && stackTypeFilter === 'All') {
+          return item.Rarity === rarityFilter
         } else {
           //The 'All' option is defaulted for both drop downs
-          return item !== null;
+          return item !== null
         }
       })
       .map((item) => (
         <Item key={item.Id} item={item} game={game} />
       ))}
   </Grid>
-);
+)
 
-export default ItemList;
+export default ItemList
